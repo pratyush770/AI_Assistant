0570b9f5ec928f21b557a964c41c0774a8962a92
pratyush majumdar <pratyushmajumdar27@gmail.com>
2025-02-03 14:19:14 +0530

    ai assistant

--- grammar_check.py
+++ grammar_check.py
@@ -2,7 +2,6 @@
 import os  # for setting environment variable
 import streamlit as st
 from langchain_core.prompts import PromptTemplate  # for defining a fixed prompt
-from langchain.schema.runnable import RunnableSequence  # for sequencing the flow
 from langchain_groq import ChatGroq  # for using llm
 
 sec_key = st.secrets["GROQ_API_KEY"]
@@ -16,7 +15,7 @@ model_name = "mixtral-8x7b-32768"  # name of model used
 llm = ChatGroq(
     model_name=model_name,
     temperature=0.6,  # more accurate results
-    groq_api_key=sec_key
+    groq_api_key=sec_key,
 )
 
 conversation_history = []  # initialize conversation history
@@ -25,14 +24,14 @@ conversation_history = []  # initialize conversation history
 def grammar_check(query):  # function to check grammatical errors
     global conversation_history  # access the global conversation history
     history = "\n".join([f"User: {q}\nAI: {r}" for q, r in conversation_history])
-    template = f"""  
+    template = """  
     {history}
     User: {query}
     AI: Check for grammatical errors and provide the most relevant and concise answer.
     """
-    prompt = PromptTemplate(template=template, input_variables=["query"])
-    sequence = RunnableSequence(first=prompt, last=llm)
-    response = sequence.invoke({"query": query})
+    prompt_template = PromptTemplate(template=template, input_variables=["history", "query"])
+    sequence = prompt_template | llm
+    response = sequence.invoke({"history": history, "query": query})
     response_text = response.content.strip()
     conversation_history.append((query, response_text))  # update conversation history
     return response_text  # return only the content
