9c1a88b6ba4f3a0f8f1e6de6d4fc22fb185bf5e8
pratyush majumdar <pratyushmajumdar27@gmail.com>
2025-04-16 19:11:11 +0530

    ai assistant

--- functions/exam_tutor.py
+++ functions/exam_tutor.py
@@ -5,11 +5,7 @@ from langchain_core.prompts import PromptTemplate  # for defining a fixed prompt
 from groq import Groq  # for using LLM
 
 sec_key = st.secrets["GROQ_API_KEY"]
-langsmith_sec_key = st.secrets['LANGCHAIN_API_KEY']
 os.environ['GROQ_API_KEY'] = sec_key  # secret_key set as environment variable
-os.environ['LANGCHAIN_API_KEY'] = langsmith_sec_key
-os.environ['LANGCHAIN_TRACING_V2'] = "true"
-os.environ['LANGCHAIN_PROJECT'] = "AI Assistant"
 
 client = Groq(api_key=sec_key)  # initialize the groq client
 conversation_history = []  # store conversation history
@@ -29,7 +25,7 @@ def generate_question_and_answers(query):  # function to generate prompt
     final_prompt = prompt_template.format(history=history, query=query)
     # call the groq llm api
     response = client.chat.completions.create(
-        model= "llama-3.1-8b-instant",
+        model= "meta-llama/llama-4-scout-17b-16e-instruct",
         messages=[{"role": "system", "content": final_prompt}],
         temperature=0,
         max_tokens=1024,
