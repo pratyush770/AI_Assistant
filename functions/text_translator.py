2bdf4a5b2a6127002c11f11fceccc947f72201e3
Pratyush Majumdar <pratyushmajumdar27@gmail.com>
2025-06-07 19:54:00 +0530

    ai assistant

--- functions/text_translator.py
+++ functions/text_translator.py
@@ -1,8 +1,14 @@
 # from secret_key import sec_key  # secret_key used for API call
 import streamlit as st
 import os  # for setting environment variable
-from langchain_core.prompts import PromptTemplate  # for defining a fixed prompt
 from langchain_groq import ChatGroq  # for using LLM
+from langgraph.graph import StateGraph, START, END
+from typing import Annotated, Sequence
+from pydantic import BaseModel
+from langgraph.graph.message import add_messages
+from langchain_core.messages import BaseMessage, SystemMessage, AIMessage
+from langgraph.prebuilt import ToolNode
+from functions.tools import duckduckgosearch
 
 sec_key = st.secrets["GROQ_API_KEY"]
 # os.environ['GROQ_API_KEY'] = sec_key  # secret_key set as environment variable
@@ -11,38 +17,78 @@ os.environ["LANGSMITH_API_KEY"] = langsmith_sec_key
 os.environ['LANGCHAIN_TRACING_V2'] = "true"
 os.environ['LANGCHAIN_PROJECT'] = "AI Assistant"
 
-model_name = "gemma2-9b-it"
+
+class AgentState(BaseModel):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    target_lang: str
+
+
+tools = [duckduckgosearch]
+
+model_name = "qwen-qwq-32b"
 llm = ChatGroq(  # create the llm
     model_name=model_name,
     temperature=0,
     groq_api_key=sec_key,
     stop=None,
     model_kwargs={"top_p": 0},
-    max_tokens=1024
+    max_tokens=None,
+    timeout=None,
+    max_retries=2
+).bind_tools(tools)
+
+
+def generate_prompt(state: AgentState) -> AgentState:
+    """ Function to generate prompt """
+    system_prompt = SystemMessage(
+        content=f"You are a helpful text translator. Use the provided tool to fetch real-time data when necessary. If no tool is required, translate the text into {state.target_lang}. Strictly provide only the translated text"
+    )
+    message = [system_prompt] + state.messages
+    response = llm.invoke(message)
+    return {"messages": [response]}
+
+
+def should_continue(state: AgentState) -> str:
+    """ Function to decide the next flow of execution """
+    messages = state.messages
+    last_message = messages[-1]  # get the most recent message
+    if not last_message.tool_calls:  # if there is no further tool calls
+        return "response"  # return the edge
+    else:
+        return "continue"
+
+
+graph = StateGraph(AgentState)
+graph.add_node("llm", generate_prompt)
+tool_node = ToolNode(tools=tools)  # create ToolNode
+graph.add_node("tools", tool_node)
+graph.add_edge(START, "llm")
+graph.add_conditional_edges(
+    "llm",
+    should_continue,
+    {
+        "continue": "tools",
+        "response": END
+    }
 )
+graph.add_edge("tools", "llm")  # goes back from tool node to our agent node
+app = graph.compile()
 
-conversation_history = []  # store conversation history
 
-
-def translate_text(query, target_lang):  # function to translate the text
-    global conversation_history  # access the global conversation history
-    MAX_HISTORY = 3
-    conversation_history = conversation_history[-MAX_HISTORY:]  # keep only the last 3 exchanges to reduce token usage
-    history = "\n".join([f"User: {q}\nAI: {r}" for q, r in conversation_history])
-    template = f"""
-    {history}
-    Question: {query}
-    Translate the following text into {target_lang}.
-    Provide only the translated text.
-    """
-
-    prompt_template = PromptTemplate(template=template, input_variables=["history", "query", "target_lang"])
-    sequence = prompt_template | llm
-    response_text = sequence.invoke({"history": history, "query": query}).content.strip()
-    conversation_history.append((query, response_text))  # update conversation history
-    return response_text
+def translate_text(query, target_lang):
+    inputs = {"messages": query, "target_lang": target_lang}  # prepare the input for the agent
+    result = app.invoke(inputs)  # invoke the agent with the updated chat history
+    # extract the AI's response
+    ai_messages = [
+        message.content if isinstance(message, AIMessage) else str(message)
+        for message in result["messages"]
+        if isinstance(message, AIMessage)
+    ]
+    recent_message = ai_messages[-1]  # get the recent message
+    return recent_message  # return the most recent AI response
 
 
 if __name__ == "__main__":
     print(translate_text("I like to watch anime", "Japanese"))  # for testing purposes
+
 
