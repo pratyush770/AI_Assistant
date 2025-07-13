90012b913ceae096b2143604812f165f6a9f33d8
Pratyush Majumdar <pratyushmajumdar27@gmail.com>
2025-07-13 23:19:14 +0530

    ai assistant

--- functions/exam_tutor.py
+++ functions/exam_tutor.py
@@ -1,16 +1,16 @@
-from secret_key import sec_key  # secret_key used for API call
-# import streamlit as st
+# from secret_key import sec_key  # secret_key used for API call
+import streamlit as st
 import re
 import os  # for setting environment variable
 from langchain_core.prompts import PromptTemplate  # for defining a fixed prompt
 from langchain_groq import ChatGroq  # for using LLM
 
-# sec_key = st.secrets["GROQ_API_KEY"]
-os.environ['GROQ_API_KEY'] = sec_key  # secret_key set as environment variable
-# langsmith_sec_key = st.secrets["LANGSMITH_API_KEY"]
-# os.environ["LANGSMITH_API_KEY"] = langsmith_sec_key
-# os.environ['LANGCHAIN_TRACING_V2'] = "true"
-# os.environ['LANGCHAIN_PROJECT'] = "AI Assistant"
+sec_key = st.secrets["GROQ_API_KEY"]
+# os.environ['GROQ_API_KEY'] = sec_key  # secret_key set as environment variable
+langsmith_sec_key = st.secrets["LANGSMITH_API_KEY"]
+os.environ["LANGSMITH_API_KEY"] = langsmith_sec_key
+os.environ['LANGCHAIN_TRACING_V2'] = "true"
+os.environ['LANGCHAIN_PROJECT'] = "AI Assistant"
 
 model_name = "meta-llama/llama-4-scout-17b-16e-instruct"  # model name
 llm = ChatGroq(  # create the llm
